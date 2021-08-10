e7fe99f005065cede4111c9d172817dd26fa72b8
KarasawaTakahiro <karasawa@basicinc.jp>
2021-08-10 20:15:00 +0900

    chore: add sample main code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate awskey;
+use awskey::services::profile_service::ProfileService;
+
 fn main() {
     println!("Hello, world!");
+
+    let profile = ProfileService::build(
+        "name".to_string(),
+        "serial_number".to_string(),
+        "arn:aws:iam:123456789012:role/HOGE-fuga_bar".to_string(),
+        "123456".to_string(),
+    );
+
+    println!("{:?}", profile);
 }
